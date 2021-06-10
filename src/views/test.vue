e1daabe7d2e38681a5d6e2828a1d63c7e729aaaa
楚呈祥 <chuchengxiang@ipfsforce.com>
2021-06-10 15:29:26 +0800

    test

---
+++ src/views/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class=el-button>
+    test
+  </div>
+</template>
+<script>
+export default {
+  name: "test",
+  components: {},
+};
+</script>
+<style scoped>
+.not-found {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+</style>>
